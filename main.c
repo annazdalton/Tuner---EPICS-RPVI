c1e604e021b6684feb9df55245790f519e828a77
Anna Dalton <annazd06@gmail.com>
2025-09-17 18:27:52 -0400

    set up main file

---
+++ main.c
@@ -0,0 +1,4 @@
+#include <math.h>
+
+#define SAMPLE_SIZE //add
+#define SAMPLING_FREQ //add 
--- main.py
+++
@@ -1,8 +0,0 @@
-import math
-import numpy as np
-from scipy import signal # signal processing library
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
-from scipy.signal import butter, lfilter
-
-
