b019e23e2aab5b38943ccd5a4b269018de2be50a
Anna Dalton <annazd06@gmail.com>
2025-09-22 19:06:36 -0400

    creating files needed for software

---
+++ audio_io.c
@@ -0,0 +1,21 @@
+// function name: load_audio_file
+// this function loads in an audio file 
+// INPUTS -----------------------------
+// char* filename - file name
+// double** samples - 
+// int num_samples - 
+// int sample_rate - 
+// RETURNS: 
+// FUNCTION CALL: 
+// int load_audio_file(const char* filename, double** samples, int* num_samples, int* sample_rate);
+
+int load_audio_file(const char* filename, double** samples, int* num_samples, int* sample_rate) 
+{
+    
+}
+
+
+int save_audio_file(const char* filename, double* samples, int num_samples, int sample_rate);
+
+
+void free_audio_data(double* samples);
---
+++ audio_io.h
@@ -0,0 +1,6 @@
+#include "audio_io.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
