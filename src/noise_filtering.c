834806662f9ee2050302b5653a56834506a990b5
Anna Dalton <annazd06@gmail.com>
2025-11-19 18:54:20 -0500

    wrote insertion filtering function

---
+++ src/noise_filtering.c
@@ -0,0 +1,33 @@
+//noise filtering functions
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+// insertion sort (for median filter)
+float insertion_sort(float *buffer, int size) {
+    for (int i = 1; i < size; i++) {
+        float key = buffer[i];
+        int j = i - 1;
+        
+        while (j >= 0 && buffer[j] > key) {
+            buffer[j + 1] = buffer[j];
+            j--;
+        }
+        buffer[j + 1] = key;
+    }
+
+    //return median for median sort
+    if (size % 2 == 0) {
+        return (buffer[size/2 - 1] + buffer[size/2]) / 2.0f;
+    } else {
+        return buffer[size/2];
+    }
+}
+
+
+// MEDIAN FILTER
+// window size: smaller window size - less smoothing of data, bigger window size - more smoothing of data
+void median_filter() {
+
+
+}
