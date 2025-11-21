df0a6ccf90a73f97c1df17cedc4b81008e59ced4
Anna Dalton <annazd06@gmail.com>
2025-11-21 11:30:38 -0500

    changed insertion sort function name and wrote median filter function

--- src/noise_filtering.c
+++ src/noise_filtering.c
@@ -4,7 +4,7 @@
 #include <string.h> 
 
 // insertion sort (for median filter)
-float insertion_sort(float *buffer, int size) {
+float find_median(float *buffer, int size) {
     for (int i = 1; i < size; i++) {
         float key = buffer[i];
         int j = i - 1;
@@ -27,7 +27,31 @@ float insertion_sort(float *buffer, int size) {
 
 // MEDIAN FILTER
 // window size: smaller window size - less smoothing of data, bigger window size - more smoothing of data
-void median_filter(const float *input, float *output, int length, int window) {
+void median_filter(const float *input, float *output, int length, int window_size) {
+    if (window_size % 2 == 0) {
+        window_size++; 
+    }
 
+    int half_window =  window_size / 2;
 
+    float *window_buffer = (float *)malloc(window_size * sizeof(float));
+    if(!window_buffer) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return -1;
+    }
+
+    //iterate through every element of audio data array
+    for (int i = 0; i < length; i++) {
+        int actual_window_size = 0; 
+
+        //stores neighboring data points of current index in window buffer
+        for (int j = -half_window; j <= half_window; j++) {
+            int index = i + j;
+            
+            if (index >= 0 && index < length) {
+                window_buffer[actual_window_size++] = input[index];
+            }
+        }
+        output[i] = find_median(window_buffer, actual_window_size);
+    }
 }
